c804878f2c2f93b141e0aa975c8ae88df06e40ad
chri4257 <33205104+chri4257@users.noreply.github.com>
2019-01-06 14:02:26 +0000

    Updated project
    Recycle tiles that have fallen

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -9,13 +9,69 @@ public class TileManager : MonoBehaviour {
 	// The current tile object in game
 	public GameObject currentTile;
 
-	// Use this for initialization
-	void Start () {
-		for (int i = 1; i < 10; i++)
+
+	private static TileManager instance;
+	public static TileManager Instance
+	{
+		get
 		{
-			SpawnTile();
+			if (instance == null)
+			{
+
+				instance = GameObject.FindObjectOfType<TileManager>();
+			}
+			return instance;
 		}
 	}
+
+    // name variables and thenassign memory to them using new
+    private Stack<GameObject> leftTiles = new Stack<GameObject>();
+    public Stack<GameObject> LeftTiles
+    {
+        get
+        {
+            return leftTiles;
+        }
+        set
+        {
+            leftTiles = value;
+        }
+    }
+
+ 
+    private Stack<GameObject> topTiles = new Stack<GameObject>();
+    public Stack<GameObject> TopTiles
+    {
+        get
+        {
+            return topTiles;
+        }
+        set
+        {
+            topTiles = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        /*
+        GameObject second = null;
+        leftTiles.Push(currentTile);
+        leftTiles.Push(second)
+        // This would look at the top tile which = second
+        GameObject tileOnTop = leftTiles.Peek();
+        // This would takes second out of the stack
+        GameObject lastRemoved = leftTiles.Pop();
+        */
+        CreateTiles(50);
+        for (int j = 0; j < 10;  j++)
+        {
+            SpawnTile();
+        }
+
+
+    }
 
 	// Update is called once per frame
 	void Update () {
@@ -24,12 +80,53 @@ public class TileManager : MonoBehaviour {
 
 	public void SpawnTile()
 	{
+
+        // See if we have enough tiles
+        if (LeftTiles.Count == 0 || TopTiles.Count == 0)
+        {
+            CreateTiles(10);
+        }
+
 		// Upper number not included in the range.
 		int randomIndex = Random.Range(0, 2);
-		// Instantiate takes a prefab and adds it to the game world
-		currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex], currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position, Quaternion.identity);
+
+
+        if (randomIndex == 0)
+        {
+            // Removes the top of the leftTiles stack and sets it = tmp
+            GameObject tmp = LeftTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+            currentTile = tmp;
+        }
+        else if (randomIndex == 1)
+        {
+            // Removes the top of the leftTiles stack and sets it = tmp
+            GameObject tmp = TopTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+            currentTile = tmp;
+        }
+
+        // Instantiate takes a prefab and adds it to the game world
 		// The below code does not work because instantiate creates an Object and currentTile is a GameObject
 		// Hence that is why we need to change the type of Object at the start with (GameObject)
 		//currentTile = Instantiate(tilePrefab[0], currentTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
 	}
+
+    public void CreateTiles(int amount)
+    {
+        for (int i = 1; i < amount; i++)
+        {
+            LeftTiles.Push(Instantiate(tilePrefabs[0]));
+            TopTiles.Push(Instantiate(tilePrefabs[1]));
+            // We name the tiles here and refer to this in the TileScript 
+            TopTiles.Peek().name = "TopTile";
+            LeftTiles.Peek().name = "LeftTile";
+            TopTiles.Peek().SetActive(false);
+            LeftTiles.Peek().SetActive(false);
+
+
+        }
+    }
 }
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Manage when to spawn tiles
+public class TileScript : MonoBehaviour {
+
+    private float fallDelay = 1.5f;
+    private float despawnDelay = 2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// When something exits the tile
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "player")
+		{
+			Debug.Log("SpawnTile");
+			// Can access SpawnTile because it is public
+			TileManager.Instance.SpawnTile();
+            // We start the coroutine script FallDown
+            StartCoroutine(FallDown());
+		}
+     
+	}
+
+    // This runs as a seperate script. It waits a few seconds then it makes the cubes fall.
+    IEnumerator FallDown()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        GetComponent<Rigidbody>().isKinematic = false;
+        yield return new WaitForSeconds(despawnDelay);
+        switch (gameObject.name)
+        {
+            // We name these in the TileManager script
+            case "LeftTile":
+                TileManager.Instance.LeftTiles.Push(gameObject);
+                gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                break;
+
+            case "TopTile":
+                TileManager.Instance.TopTiles.Push(gameObject);
+                gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                break;
+
+        }
+    }
+}
